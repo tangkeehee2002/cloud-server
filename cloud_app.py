667e38ce049b174b2eb7a6ebb33e211dfcdc9a99
r3ddy97 <venkyreddy097@gmail.com>
2018-03-30 11:44:10 +0530

    jinja2 to update file systems

--- cloud_app.py
+++ cloud_app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 '''handles server side database'''
 
-import os
+from os import (mkdir, listdir, stat, system)
+from os.path import (abspath, exists)
 # from subprocess import (Popen, PIPE)
-# import datetime
-import crypt
+from crypt import crypt
 from hmac import compare_digest as compare_hash
 from shutil import copytree
 from time import ctime
@@ -15,7 +15,6 @@ import server
 
 # REDIS_OBJ = redis.StrictRedis("", port=6379)
 REDIS_OBJ = redis.StrictRedis()
-CWD = os.getcwd()
 
 
 def handle_login(request, response, login_details):
@@ -24,10 +23,9 @@ def handle_login(request, response, login_details):
     hashed_passwd = REDIS_OBJ.get(username)
     if hashed_passwd:
         hashed_passwd = hashed_passwd.decode()
-        if compare_hash(hashed_passwd, crypt.crypt(passwd, hashed_passwd)):
+        if compare_hash(hashed_passwd, crypt(passwd, hashed_passwd)):
             redir_path = "user/{}/home.html".format(username).encode()
             # print(redir_path)
-
             # server.redirect(request, response, redirected_path, 307)
             return read_html("static/home.html").replace(b"{redirect}", redir_path)
     return read_html("static/failed_login.html").replace(b'{reason}', b'wrong credentials')
@@ -41,7 +39,7 @@ def handle_signup(request, response, signup_details):
         res_page = read_html('static/failed_signup.html')
         return res_page.replace(b"{reason}", validity_status[1])
     username, password = signup_details["username"], signup_details["password"]
-    signup_details["password"] = crypt.crypt(password)
+    signup_details["password"] = crypt(password)
     # detail_names = [fname, lname, email, username, hashed_passwd]
     user_id = REDIS_OBJ.incr("users")
     REDIS_OBJ.hmset(name="user:{}".format(user_id), mapping=signup_details)
@@ -51,11 +49,11 @@ def handle_signup(request, response, signup_details):
     REDIS_OBJ.set(username, signup_details["password"])
     REDIS_OBJ.save()
     # REDIS_OBJ.set(username, userid)
-    src = os.path.abspath("static/login")
-    dst = os.path.abspath("static/user/{}".format(username))
-    if not os.path.exists(dst):
+    src = abspath("static/login")
+    dst = abspath("static/user/{}".format(username))
+    if not exists(dst):
         copytree(src, dst)
-    os.mkdir("{}/uploads".format(dst))
+    mkdir("{}/uploads".format(dst))
     # redirected_path = "user/{}/home.html".format(username)
     # server.redirect(request, response, redirected_path, 307)
     return read_html("static/user/{}/index.html".format(username))# return login page again
@@ -63,11 +61,10 @@ def handle_signup(request, response, signup_details):
 
 def read_html(html):
     # print("read_html")
-    html_abspath = os.path.abspath(html)
+    html_abspath = abspath(html)
     with open(html_abspath, "rb") as page:
         html_bytes = page.read()
         return html_bytes
-
 
 def signup_validity(signup_details):
     # print("signup_validity")
@@ -87,24 +84,28 @@ def signup_validity(signup_details):
 def save_uploads(request, response, parsed_request_body):
     # parsed_request_body = request["body"]
     user = request["header"]["Referer"].split("/")[-2]
-    # user, _ = post_from
-    user_dir = 'static/user/{}/'.format(user)
-    upload_dir = "{}uploads/".format(user_dir)
-    for file in parsed_request_body:
-        filename = os.path.abspath(upload_dir + file)
+    upload_dir = "static/user/{}/uploads/".format(user)
+    for file, body in parsed_request_body.items():
+        filename = abspath(upload_dir + file)
         with open(filename, "wb") as fname:
-            fname.write(parsed_request_body[file]["body"])
+            fname.write(body["body"])
 
-        # server.redirect(request, response, redirected_path, 307)
-    user_files = [upload_dir+f for f in os.listdir(upload_dir)]
-    # user_files = [f for f in os.listdir(upload_dir)]
-    # os.chdir(upload_dir)
-    user_file_stats = []
-    for  file in user_files:
-        fstat = os.stat(os.path.abspath(file))
-        user_file_stats.append([file, (fstat.st_size)/1000, ctime(fstat.st_ctime)])
-    print(user_file_stats)
+    # server.redirect(request, response, redirected_path, 307)
+    file_page = "static/user/{}/files.html".format(user)
+    update_filepage(upload_dir, file_page)
     return read_html("static/user/{}/files.html".format(user))
+
+def update_filepage(upload_dir, file_page):
+    file_list = listdir(upload_dir)
+    fstat_list = []
+    for  num, fname in enumerate(file_list):
+        fstat = stat(abspath(upload_dir +"/"+ fname))
+        fstat_list.append([num+1, fname, fstat.st_size/1000, ctime(fstat.st_ctime)])
+    print(fstat_list)
+    template = Template(read_html("static/files_template.html").decode())
+    rendered_fpage = template.render(flist_template=fstat_list).encode()
+    with open(file_page, "wb+") as fpage:
+        fpage.write(rendered_fpage)
 
 
 def handle_entry(request, response, parsed_request_body):
@@ -131,9 +132,8 @@ def handle_post(request, response):
 def main():
     # Popen(["redis-server"], stdout=PIPE, stderr=PIPE,)
     server.add_route("POST", handle_post)
-
     server.execute_server()
 
 if __name__ == '__main__':
-    os.system("clear||cls")
+    system("clear||cls")
     main()
